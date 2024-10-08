ec0fcae5d18ed7c8b7e87bbd580149322be14c00
Muhammed Mustafa Özdemir <71263282+Clavatum@users.noreply.github.com>
2024-10-08 16:20:58 +0300

    tower mechanic is updated

--- 1st/Assets/Assets/Scripts/BuildingMng/BuildingMng.cs
+++ 1st/Assets/Assets/Scripts/BuildingMng/BuildingMng.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class BuildingMng : MonoBehaviour
 {
@@ -17,28 +18,31 @@ public class BuildingMng : MonoBehaviour
     private Transform currentTowerPreview;
     private bool isPlacingTower = false;
 
+    private BoxCollider towerCollider; 
+
+    private GameObject selectedTower;  
+    private bool isPanelActive = false;  
+
+    private GameObject towerUIPanel;  
+    private Button produceUnitButton; 
+    private Button upgradeTowerButton; 
+
     private void Awake()
     {
         towerInputActions = new TowerInputActions();
-
         towerInputActions.View.View.performed += e => inputView = e.ReadValue<Vector2>();
-
         towerInputActions.Enable();
-    }
-
-    private void Start()
-    {
-
     }
 
     void Update()
     {
         if (isPlacingTower)
         {
-            MoveTowerWithMouse();  
+            MoveTowerWithMouse();
         }
 
-        InstantiateTower(); 
+        InstantiateTower();
+        DetectTowerClick();  
     }
 
     private Vector3 GetMouseWorldPosition()
@@ -49,10 +53,10 @@ public class BuildingMng : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit))
         {
-            return hit.point; 
+            return hit.point;
         }
 
-        return Vector3.zero; 
+        return Vector3.zero;
     }
 
     private void MoveTowerWithMouse()
@@ -61,7 +65,6 @@ public class BuildingMng : MonoBehaviour
 
         if (mouseWorldPosition != Vector3.zero && currentTowerPreview != null)
         {
-
             currentTowerPreview.position = new Vector3(mouseWorldPosition.x, (currentTowerPreview.localScale.y / 2), mouseWorldPosition.z);
         }
     }
@@ -72,6 +75,11 @@ public class BuildingMng : MonoBehaviour
         {
             if (mouseWorldPosition != Vector3.zero)
             {
+                if (towerCollider != null)
+                {
+                    towerCollider.enabled = true;
+                }
+
                 isPlacingTower = false;
                 currentTowerPreview = null;
             }
@@ -82,7 +90,7 @@ public class BuildingMng : MonoBehaviour
     {
         if (currentTowerPreview != null)
         {
-            Destroy(currentTowerPreview.gameObject); 
+            Destroy(currentTowerPreview.gameObject);
         }
 
         switch (towerType)
@@ -98,20 +106,63 @@ public class BuildingMng : MonoBehaviour
                 break;
         }
 
+        towerCollider = currentTowerPreview.GetComponent<BoxCollider>();
+
+        if (towerCollider != null)
+        {
+            towerCollider.enabled = false; 
+        }
+
         isPlacingTower = true;
     }
-   
-    #region - Enable/Disable -
 
-    private void OnEnable()
+    private void DetectTowerClick()
     {
-        towerInputActions.Enable();
+        if (towerInputActions.Actions.MouseLeft.triggered)
+        {
+            Ray ray = mapCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null && hit.collider.gameObject.CompareTag("Tower"))  
+                {
+                    selectedTower = hit.collider.gameObject;
+
+                    towerUIPanel = selectedTower.transform.Find("Canvas/TowerUIPanel")?.gameObject;
+
+                    produceUnitButton = towerUIPanel.transform.Find("ProduceUnitButton")?.GetComponent<Button>();
+                    upgradeTowerButton = towerUIPanel.transform.Find("UpgradeTowerButton")?.GetComponent<Button>();
+
+                    produceUnitButton.onClick.RemoveAllListeners();  
+                    produceUnitButton.onClick.AddListener(() => ProduceUnit());
+
+                    upgradeTowerButton.onClick.RemoveAllListeners();  
+                    upgradeTowerButton.onClick.AddListener(() => UpgradeTower());
+
+                    isPanelActive = !isPanelActive;
+                    towerUIPanel.SetActive(isPanelActive);
+
+                    if (isPanelActive)
+                    {
+                        Vector3 panelPosition = selectedTower.transform.position + new Vector3(0, 2, 0);  
+                        towerUIPanel.transform.position = panelPosition;
+
+                        towerUIPanel.transform.LookAt(mapCamera.transform);
+                        towerUIPanel.transform.Rotate(0, 180, 0); 
+                    }
+                }
+            }
+        }
     }
 
-    private void OnDisable()
+    private void ProduceUnit()
     {
-        towerInputActions.Disable();
+        Debug.Log("Units produced");
     }
 
-    #endregion
+    private void UpgradeTower()
+    {
+        Debug.Log("Tower updated");
+    }
 }
