299cb29ad3e006c2706870debbc97036202921a8
Muhammed Mustafa Özdemir <71263282+Clavatum@users.noreply.github.com>
2024-10-29 21:36:08 +0300

    Auth. is updated

--- 1st/Assets/Assets/Scripts/AuthCheck.cs
+++ 1st/Assets/Assets/Scripts/AuthCheck.cs
@@ -1,43 +1,109 @@
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
 public class AuthCheck : MonoBehaviour
 {
     public TMP_Text logTxt;
+    public TMP_Text statusTxt; // Status göstermek için yeni text
+    private bool isCheckingConnection = false;
+    private bool isConnectionLost = false;
+    private float countdownTime = 5f; // Geri sayým süresi
 
-    private void Update()
+    private void Start()
     {
-        CheckInternetConnection();
+        statusTxt.text = "Status: Online"; // Baþlangýç durumu
+        StartCoroutine(CheckInternetConnectionRoutine());
     }
 
-    void CheckInternetConnection()
+    IEnumerator CheckInternetConnectionRoutine()
     {
-        if (Application.internetReachability == NetworkReachability.NotReachable)
+        while (true)
         {
-            logTxt.gameObject.SetActive(true);
-            Debug.Log("No Internet Connection");
-            logTxt.text = "Internet connection lost!";
-            ShowErrorAndReturnToSignIn(); 
+            if (!isCheckingConnection)
+            {
+                StartCoroutine(CheckInternetConnection());
+            }
+            yield return new WaitForSeconds(1f); // Her saniye baðlantýyý kontrol eder
         }
     }
 
-    void LoadGameSceneByIndex(int sceneIndex)
+    IEnumerator CheckInternetConnection()
     {
-        SceneManager.LoadScene(sceneIndex);
+        isCheckingConnection = true;
+
+        using (UnityWebRequest request = UnityWebRequest.Get("https://www.google.com"))
+        {
+            request.timeout = 5;
+
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                if (!isConnectionLost)
+                {
+                    OnConnectionLost(); // Baðlantý ilk kez kaybedildiðinde baþlat
+                }
+            }
+            else
+            {
+                if (isConnectionLost)
+                {
+                    OnConnectionRestored(); // Baðlantý tekrar geldiðinde
+                }
+            }
+        }
+
+        isCheckingConnection = false;
     }
 
-    void ShowErrorAndReturnToSignIn()
+    void OnConnectionLost()
     {
-        logTxt.text += "\nReturning to sign-in screen...";
-        Invoke("ReturnToSignIn", 2f); 
+        isConnectionLost = true;
+        Time.timeScale = 0; // Oyunu duraklatýr
+        statusTxt.text = "Status: Connection Lost";
+        countdownTime = 5f; // Geri sayýmý baþlat
+        StartCoroutine(CountdownToReturn());
+    }
+
+    void OnConnectionRestored()
+    {
+        isConnectionLost = false;
+        Time.timeScale = 1; // Oyunu devam ettirir
+        statusTxt.text = "Status: Online";
+        logTxt.gameObject.SetActive(false); // Hata mesajýný gizler
+        StopCoroutine(CountdownToReturn()); // Geri sayýmý durdurur
+    }
+
+    IEnumerator CountdownToReturn()
+    {
+        logTxt.gameObject.SetActive(true);
+
+        while (countdownTime > 0)
+        {
+            logTxt.text = $"Internet connection lost! Returning in {countdownTime:F0} seconds...";
+            yield return new WaitForSecondsRealtime(1f); // Her saniye günceller
+            countdownTime--;
+
+            // Baðlantý geri geldiyse geri sayýmý durdurur
+            if (!isConnectionLost)
+            {
+                yield break;
+            }
+        }
+
+        // Geri sayým bittiðinde giriþ ekranýna döner
+        if (isConnectionLost)
+        {
+            ReturnToSignIn();
+        }
     }
 
     void ReturnToSignIn()
     {
-        LoadGameSceneByIndex(0);
+        SceneManager.LoadScene(0); // Giriþ ekranýna döner
         logTxt.gameObject.SetActive(false);
     }
 }
